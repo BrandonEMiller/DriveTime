2eb424336455cdec76b26baa253ca5326a34b86c
Brandon Miller <brandonevanmiller@gmail.com>
2013-10-15 17:19:34 -0400

    Hooked up google maps api and began using hardcoded trip inputs to get duration values in seconds to appear

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+var directionsService = new google.maps.DirectionsService();
+     var directionsDisplay = new google.maps.DirectionsRenderer();
+
+
+     var request = {
+       origin: '405 New Tarleton Way, greer, sc', 
+       destination: '110 Watermill Road, greer, sc',
+       travelMode: google.maps.DirectionsTravelMode.DRIVING
+     };
+
+     directionsService.route(request, function(response, status) {
+       if (status == google.maps.DirectionsStatus.OK) {
+         directionsDisplay.setDirections(response);
+         var route = response.routes[0];
+         console.log(route.legs[0].duration.value/60)
+       }
+     });
