3f1477dcc4e81c46c159b013062b742234e899a8
Brandon Miller <brandonevanmiller@gmail.com>
2013-10-15 18:10:41 -0400

    Made user inputs for street, city, and state of origin and destination and continued calculating minutes of travel with google's api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,25 @@
-var directionsService = new google.maps.DirectionsService();
-     var directionsDisplay = new google.maps.DirectionsRenderer();
+$(".time").click(function(){
+	var directionsService = new google.maps.DirectionsService();
+	     var directionsDisplay = new google.maps.DirectionsRenderer();
 
 
-     var request = {
-       origin: '405 New Tarleton Way, greer, sc', 
-       destination: '110 Watermill Road, greer, sc',
-       travelMode: google.maps.DirectionsTravelMode.DRIVING
-     };
+	     var request = {
+	       origin: $('.origin-street').val() +', '+ $('.origin-city').val() +', ' + $('.origin-state').val(), 
+	       destination: $('.destination-street').val() +', '+ $('.destination-city').val() +', ' + $('.destination-state').val(), 
+	       travelMode: google.maps.DirectionsTravelMode.DRIVING
+	     };
+	     console.log (request.origin)
+	     console.log($('.destination-street').val())
+	     directionsService.route(request, function(response, status) {
+	       if (status == google.maps.DirectionsStatus.OK) {
+	         directionsDisplay.setDirections(response);
+	         var route = response.routes[0];
+	         console.log(route.legs[0].duration.value/60)
+	         $('.hero-unit').append(route.legs[0].duration.value/60 + ' minutes')
+	       }
 
-     directionsService.route(request, function(response, status) {
-       if (status == google.maps.DirectionsStatus.OK) {
-         directionsDisplay.setDirections(response);
-         var route = response.routes[0];
-         console.log(route.legs[0].duration.value/60)
-       }
-     });
+	       else {
+	       	console.log("Error")
+	       }
+	     });
+});
