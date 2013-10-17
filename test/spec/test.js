edd8bc8b30e7a6d080902bb37a7505eb607d6e5d
Brandon Miller <brandonevanmiller@gmail.com>
2013-10-17 07:55:06 -0400

    Wrote a few tests to make sure wercker is functioning correctly

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,9 +3,22 @@
 (function () {
     describe('Give it some context', function () {
         describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
 
-            });
+             it('confirm jQuery is avaliable', function () {
+        		expect(jQuery() !== undefined || null).to.equal(true);
+    	    });
+
+    	  	 it('confirm Underscore is avaliable', function () {
+        		expect(_.VERSION !== undefined || null).to.equal(true);
+    	    });
+
+    	  	 it('confirm Backbone is avaliable', function () {
+        		expect(Backbone.VERSION !== undefined || null).to.equal(true);
+    	    });
+
+    	  	 it('should fail here', function() {
+    	  	 	expect(route_time).to.equal(57)
+    	  	 })
         });
     });
 })();
