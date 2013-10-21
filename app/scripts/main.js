ed15911f7983a5daa3110cabf9d871b9410608dc
Brandon Miller <brandonevanmiller@gmail.com>
2013-10-21 16:30:14 -0400

    Began saving data to Parse

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -40,7 +40,6 @@ $(".time").click(function(){
 	        route_time= Math.round(route.legs[0].duration.value/60)
 	        directionsDisplay.setDirections(response);
 
-	        console.log(route_time)
 	        $('.container').append(route_time + ' minutes')
 	       }
 
@@ -56,10 +55,43 @@ $(".submit_dates").click(function(){
 	var meetingEnd = new Date (endDatePicker.get('select').year, endDatePicker.get('select').month, endDatePicker.get('select').date , endTimePicker.get('select').hour, endTimePicker.get('select').mins, 0, 0)
 	console.log(meetingStart)
 	//console.log(picker.get('select').date)
-	var leaveTimeHour = Math.floor((startTimePicker.get('select').pick - route_time)/60)
+	var diff = startTimePicker.get('select').pick - route_time
+	var leaveTimeHour = Math.floor(diff/60)
 	console.log(leaveTimeHour)
-	var leaveTimeMinute =startTimePicker.get('select').pick - route_time - leaveTimeHour*60
+	var leaveTimeMinute =diff- leaveTimeHour*60
 	console.log(leaveTimeMinute)
+	var timePeriod = "AM"
+	if((diff/60)>=12) {
+		timePeriod = "PM"
+		leaveTimeHour-=12
+		if(leaveTimeHour == 0) {
+			leaveTimeHour=12
+		}
+	}
+
 	var leaveForMeeting = "" + leaveTimeHour + ":" + leaveTimeMinute
-	$('.container').append('You need to leave at ' + leaveForMeeting)
+	$(".save_event").click(function(){
+		var Event = Parse.Object.extend("Event");
+		var driveEvent = new Event();
+		driveEvent.set("eventName", $('.event_name').val());
+		driveEvent.set("eventStartDate", startDatePicker);
+		driveEvent.set("eventStartTime", startTimePicker);
+		driveEvent.set("eventEndDate", endDatePicker);
+		driveEvent.set("eventEndTime", endTimePicker);
+		driveEvent.set("eventLeaveTime", leaveForMeeting);
+		driveEvent.set("timePeriod", timePeriod);
+
+
+		driveEvent.save(null, {
+  			success: function(eventName) {
+  			  // The object was saved successfully.
+  		},
+  			error: function(eventName, error) {
+    		// The save failed.
+    		// error is a Parse.Error with an error code and description.
+  		}
+	});
+	})	 
+	$('.container').append('You need to leave at ' + leaveForMeeting + ' ' + timePeriod)
 });	
+
