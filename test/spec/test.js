d5bd52705b0d114b1630e41fba6c19437bb219ac
Brandon Miller <brandonevanmiller@gmail.com>
2013-10-30 08:00:19 -0400

    Testing a few more things

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,8 +1,8 @@
 /*global describe, it */
 'use strict';
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
+    describe('DriveTime', function () {
+        describe('should by default', function () {
 
              it('confirm jQuery is avaliable', function () {
         		expect(jQuery() !== undefined || null).to.equal(true);
@@ -16,11 +16,42 @@
         		expect(Backbone.VERSION !== undefined || null).to.equal(true);
     	    });
 
-             
+             it('confirm that the Main View is avaliable', function() {
+                expect(MainView !== undefined || null).to.equal(true);
+             })
+             it('confirm that the Create Event View is avaliable', function() {
+                expect(CreateEventView !== undefined || null).to.equal(true);
+             })
+             it('confirm that the Create Leave Event View is avaliable', function() {
+                expect(CreateLeaveEventView !== undefined || null).to.equal(true);
+             })
+             it('confirm that the Edit View is avaliable', function() {
+                expect(EditEventView !== undefined || null).to.equal(true);
+             })
+             it('confirm that the About View is avaliable', function() {
+                expect(AboutView !== undefined || null).to.equal(true);
+             })
 
-    	  	 it('should fail here', function() {
-    	  	 	expect(route_time).to.equal(57)
-    	  	 })
+             it('should have a home route', function () {
+                expect(router.routes['']).to.equal('home')
+             })
+             it('should have a home route at /home', function () {
+                expect(router.routes['home']).to.equal('home')
+             }) 
+             it('should have a New Event route at /newEvent', function () {
+                expect(router.routes['newEvent']).to.equal('createNew')
+             }) 
+             it('should have a New Leave Event route at /new_leave_event', function () {
+                expect(router.routes['new_leave_event']).to.equal('createNewLeave')
+             }) 
+             it('should have an about route at /about', function () {
+                expect(router.routes['about']).to.equal('about')
+             }) 
+             it('should have an Edit route at /events/:_id', function () {
+                expect(router.routes['events/:_id']).to.equal('showEvent')
+             })
+
+
         });
     });
 })();
